2c61153afcb0f1b8ab1a0f84048078cae7e06163
GiseleVilarinho <GiseleVilarinho@users.noreply.github.com>
2019-12-27 15:19:09 +0000

    add

--- Examples/src/io/altar/jseproject/pratica1/models/Product.java
+++ Examples/src/io/altar/jseproject/pratica1/models/Product.java
@@ -1,25 +1,31 @@
 package io.altar.jseproject.pratica1.models;
 
-import java.util.ArrayList;
+import java.util.ArrayList; // just import a library 
 import java.util.List;
 
 public class Product {
 
 	private long id;
-	private List<Long> shelvesIds = new ArrayList<Long>();
+	private List<Long> shelvesIds = new ArrayList<Long>(); // because my object product need a list of shelves that they can be add
 	private int discount;
 	private int iva;
 	private float pvp;
 	
 	
-	public Product() {}
+	public Product() {} // constructor - TEM O MESMO NOME DA CLASSE  - i need to check if i need??
 	
-	public Product(int discount, int iva, float pvp) {
+	//Product farinha = new Product(); meu produto é agora farinha (OBJETO), isso é como eu declaro um produto
+	
+	public Product(int discount, int iva, float pvp) 
+	//overload constructor - means that between () i have the information of my new object
+	{ 
 		this.discount = discount;
 		this.iva = iva;
 		this.pvp = pvp;
 	}
-
+ // Product acai = new Product(20, 0, 5, 6, 12.1);
+	//id=20 shelves=0 discount= 5 iva =6 pvp=12.1
+			
 	public Product(long id, List<Long> shelvesIds, int discount, int iva, float pvp) {
 		this.id = id;
 		this.shelvesIds = shelvesIds;
@@ -28,11 +34,12 @@ public class Product {
 		this.pvp = pvp;
 	}
 	
-	public long getId() {
+	public long getId() {  //é um metodo que acessa a ID do objeto da minha classe Product
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(long id) { //
 		this.id = id;
+		//acai.setId(30) - setId - muda para um novo valor a id do meu produto
 	}
 	public List<Long> getShelvesIds() {
 		return shelvesIds;
@@ -65,7 +72,8 @@ public class Product {
 		this.pvp = pvp;
 	}
 
-	@Override
+	@Override  // -  notacao - means que esse metodo é acessado na classe de onde essa classe é extendida. Sobre escreve o metodo 
+	//da classe da qual foi estendia, caso decida mudar o metodo o compilador me devolve um erro. 
 	public String toString() {
 		return "Product [id=" + id + ", shelvesIds=" + shelvesIds + ", discount=" + discount + ", iva=" + iva + ", pvp="
 				+ pvp + "]";
